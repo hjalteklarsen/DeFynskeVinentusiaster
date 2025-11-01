1a8205ba2378368089f6eb1c062fdf1197792cd9
hjalteklarsen <73437236+hjalteklarsen@users.noreply.github.com>
2025-11-01 17:10:18 +0100

    Inital

---
+++ defynske/src/main/java/org/example/defynske/DefynskeApplication.java
@@ -0,0 +1,13 @@
+package org.example.defynske;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefynskeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DefynskeApplication.class, args);
+    }
+
+}
